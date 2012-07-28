32633c8e17f57cb1625e96fb3b003f25250e0c22
Tumas Bajoras <hanniph@gmail.com>
2012-07-29 01:15:41 +0300

    method to bootstrap feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,8 +5,7 @@ class Feed < ActiveRecord::Base
   validates_presence_of :url
 
   def update
-    feed = Feedzirra::Feed.fetch_and_parse(self.url)
-    check_events feed.entries
+    check_events Feed.fetch_feed(self.url).entries
   end
 
   def check_events entries
@@ -17,5 +16,21 @@ class Feed < ActiveRecord::Base
         end
       end
     end
+  end
+
+  def self.create_from_url url
+    if feed = self.fetch_feed(url)
+      Feed.create!(
+        title: feed.title,
+        description: feed.description,
+        url: feed.feed_url
+      )
+    end
+  end
+
+  private 
+
+  def self.fetch_feed url
+    Feedzirra::Feed.fetch_and_parse url
   end
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,32 +1,60 @@
 require 'spec_helper'
 
 describe Feed do
-  before do
-    @feed = FactoryGirl.create :feed
-    @event = FactoryGirl.create :event, feed: @feed
-    @user_event = FactoryGirl.create :user_upload_event, feed: @feed
-    @rss_entry = FactoryGirl.build :rss_entry
-  end
+  context "no feed exists" do
+    describe "create from url" do
+      before do
+        mock = mock()
+        mock.expects(:title).returns('foo')
+        mock.expects(:description).returns('bar')
+        mock.expects(:feed_url).returns('www.test.com')
+        Feed.expects(:fetch_feed).returns(mock)
+      end
 
-  describe "check_events" do
-    it "should call correct event checker" do
-      any_upload_event.expects(:event_occurred?).once.returns(false)
-      @feed.check_events([@rss_entry])
-    end
+      it "should create feed object" do
+        expect {
+          Feed.create_from_url 'http://localhost:3000/feed.xml'
+        }.to change { Feed.all.size }.to 1 
+      end
 
-    context 'event occurred' do
-      it "should register event" do
-        any_upload_event.expects(:event_occurred?).once.returns(true)
-        any_upload_event.expects(:create_event_record).once
-        @feed.check_events([@rss_entry])
+      it "should fill relevand fede info" do
+        Feed.create_from_url 'http://localhost:3000/feed.xml'
+        feed = Feed.first
+        feed.url.should == 'www.test.com'
+        feed.title.should == 'foo'
+        feed.description.should == 'bar'
       end
     end
+  end
 
-    context "event did not occur" do
-      it "should not register event" do
+  context "with existing feeds" do
+    before do
+      @feed = FactoryGirl.create :feed
+      @event = FactoryGirl.create :event, feed: @feed
+      @user_event = FactoryGirl.create :user_upload_event, feed: @feed
+      @rss_entry = FactoryGirl.build :rss_entry
+    end
+
+    describe "check_events" do
+      it "should call correct event checker" do
         any_upload_event.expects(:event_occurred?).once.returns(false)
-        any_upload_event.expects(:create_event_record).never
         @feed.check_events([@rss_entry])
+      end
+
+      context 'event occurred' do
+        it "should register event" do
+          any_upload_event.expects(:event_occurred?).once.returns(true)
+          any_upload_event.expects(:create_event_record).once
+          @feed.check_events([@rss_entry])
+        end
+      end
+
+      context "event did not occur" do
+        it "should not register event" do
+          any_upload_event.expects(:event_occurred?).once.returns(false)
+          any_upload_event.expects(:create_event_record).never
+          @feed.check_events([@rss_entry])
+        end
       end
     end
   end
